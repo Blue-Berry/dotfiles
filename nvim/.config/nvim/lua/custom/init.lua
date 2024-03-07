11c1ff2b0e77f420db15404f21567802f652cf7c
Liam Berry <liamandberry@gmail.com>
2024-03-07 10:46:45 +0200

    Added nvim-statup

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -27,24 +27,24 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 })
 
 -- Open nvim-tree if passed a directory
-local autocmd = vim.api.nvim_create_autocmd
-local function open_nvim_tree(data)
-  -- buffer is a directory
-  local directory = vim.fn.isdirectory(data.file) == 1
-  -- buffer is a [No Name]
-  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
-  if not directory and not no_name then
-    return
-  end
-  if directory then
-    -- change to the directory
-    vim.cmd.cd(data.file)
-  end
-  -- open the tree
-  -- require("nvim-tree.api").tree.open()
-  require("telescope.builtin").find_files()
-end
-autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+-- local autocmd = vim.api.nvim_create_autocmd
+-- local function open_nvim_tree(data)
+--   -- buffer is a directory
+--   local directory = vim.fn.isdirectory(data.file) == 1
+--   -- buffer is a [No Name]
+--   local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+--   if not directory and not no_name then
+--     return
+--   end
+--   if directory then
+--     -- change to the directory
+--     vim.cmd.cd(data.file)
+--   end
+--   -- open the tree
+--   -- require("nvim-tree.api").tree.open()
+--   require("telescope.builtin").find_files()
+-- end
+-- autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 -- Remember last cursor position
 local lastplace = vim.api.nvim_create_augroup("LastPlace", {})
--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -158,5 +158,20 @@ local plugins = {
     "RRethy/vim-illuminate",
     lazy = false,
   },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  },
+  {
+    "startup-nvim/startup.nvim",
+    requires = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    lazy = false,
+    config = function()
+      require("startup").setup()
+    end,
+  },
 }
 return plugins
